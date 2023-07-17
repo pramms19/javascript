04d355fdc2d9f4e36d168d4e1092f99f3d9fdb44
Pramada Shrestha <pramadashrestha17@gmail.com>
2023-07-17 06:38:11 +0545

    file

---
+++ javascript/main.js
@@ -0,0 +1,135 @@
+//console.log('Hello from main')
+
+//varisbles
+//let a = 90
+//console.log(a)
+
+//const b = 100000
+//console.log(b)
+
+// const person = {
+//     'fname' : 'bruce',
+//     'lname': 'wayne',
+//     'age': 56,
+
+// }
+// console.log(person.fname)
+
+// const odd = [1,3,5,7,9]
+// console.log(odd[3])
+
+// let a= 'hello'
+// a=90
+// a=true
+// console.log(a)
+
+// let a = 90;
+// b = 30;
+
+// console.log(a / b);
+// console.log(++a);
+// console.log(--b);
+
+// console.log(a === b);
+// const x = a > 8 && b > 8;
+// console.log(x);
+
+// const y = a > 8 || b > 8;
+// console.log(y);
+
+// const valid = true;
+// console.log(!valid);
+
+// console.log("bruce " + "wayne");
+
+// const even = 10 % 2 === 0 ? 'even' : 'odd'
+// console.log(even)
+
+// console.log( 9 + '5')
+// console.log( true + '5')
+// console.log( '9' - '5')
+// console.log( 'bruce' - 'wayne')
+// console.log( 9 + undefined)
+
+// console.log(Number(false))// ' '
+// console.log(parseInt(6))
+// console.log(parseFloat(9.89))
+
+// console.log(String(false))
+// console.log((45).toString())
+// console.log(Boolean(' '))
+
+// const num = 0
+// if(num > 0) {
+//     console.log('positive')
+// }else if(num <0)
+// {
+//     console.log('negative')
+// }else
+// console.log('zero')
+
+// const color = 90
+
+// switch(color)
+// {
+//     case 'red':
+//     console.log('color is red')
+//     break
+
+//     case 'blue':
+//     console.log('color is bue')
+//     break
+
+//     default:
+//         console.log('not a valid color')
+
+// }
+
+// for (let i=1; i<=5; i++){
+//     console.log('iteration' +i)
+// }
+
+// let i=1
+// while (i<=5) {
+//     console.log('iteration' +i)
+//     i++
+// }
+
+// let i=6
+// do{
+//     console.log('iteration' +i)
+//     i++
+// }while (i<=5)
+
+// const array = [1,2,3,4,5]
+
+// for (const num of array){
+//     console.log('iteration number'+num)
+// } 
+
+// function groot(name){
+//     console.log('hello ' +name)
+// }
+
+// groot('riya')
+// groot('alex')
+
+// function add(a,b){
+//     return a+b
+// }
+
+// const addfive = (num) => num+5
+// const sum = add( 9,78)
+// console.log(sum)
+
+// if(true){
+//     const name= 'lalala'
+//     console.log(name)
+// }
+
+// function testfn(){
+//     const name= 'batman'
+//     console.log(name)
+// }
+
+// testfn()
